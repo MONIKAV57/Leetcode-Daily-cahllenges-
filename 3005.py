5db3c0ab0d6213ba2a3ea3087e2f53636b82fc37
Monika V <monikav5723@gmail.com>
2024-03-19 22:17:53 +0530

    changed naming

--- 3005.py
+++ 3005.py
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/count-elements-with-maximum-frequency/submissions/1197599902?envType=daily-question&envId=2024-03-08
+# https://leetcode.com/problems/count-elements-with-maximum-frequency/submissions/1197599902?envType=daily-question&envId=2024-03-08
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
     
